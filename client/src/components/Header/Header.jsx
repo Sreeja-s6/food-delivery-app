227d81dc77f2c5aae48673a89ac7ef53133a2c62
Sreeja <sreeselvaraj111@gmail.com>
2026-03-05 22:02:01 +0530

    make header section responsive

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
                 <p>
                     Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our satisfy your cravings and elevate your dining experience, one delicious meal at a time.
                 </p>
-                <button>View Menu</button>
+                <a href='#explore-menu'>View Menu</a>
             </div>
         </div>
     )
